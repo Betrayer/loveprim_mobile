f5e1ace8a23abd7a461c39d1a566fd2c0159c2c1
Betrayer <StarDestroyer@ukr.net>
2020-07-03 12:23:20 +0300

    done menu and few pages

---
+++ screens/additionalScreens/SizeChartScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, } from "react-native";
+
+
+export const SizeChartScreen = () => {
+    return (
+        <Text>
+            I am Sizing
+        </Text>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      fontFamily: "ubuntu-regular",
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
