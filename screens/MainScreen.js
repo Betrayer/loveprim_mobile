261a5e9b01e980d022260409469629aaad123a3c
Betrayer <58057555+Betrayer@users.noreply.github.com>
2020-07-03 12:24:39 +0300

    done menu and few pages (#5)

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,19 +1,27 @@
-import React from "react";
-import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-// import { AboutTab } from "./tabs/aboutTab";
-// import { BlogTab } from "./tabs/blogTab";
-// import { ServicesTab } from "./tabs/servicesTab";
-// import { MainTab } from "./tabs/mainTab";
 import { AntDesign } from "@expo/vector-icons";
-
-const Tab = createBottomTabNavigator();
+import { ProfileScreen } from "./additionalScreens/ProfileScreen";
+import { BacketScreen } from "./additionalScreens/BacketScreen";
+import { HomeScreen } from "./additionalScreens/HomeScreen";
 
 export const MainScreen = ({ navigation, route }) => {
+  const [drawer, setDrawer] = useState(false);
+  const Tab = createBottomTabNavigator();
+
+  useEffect(() => {
+    setDrawer(false);
+  }, []);
 
   navigation.setOptions({
     headerRight: () => (
@@ -24,83 +32,98 @@ export const MainScreen = ({ navigation, route }) => {
         Login
       </Text>
     ),
+    headerLeft: () => (
+      <Text style={styles.register} onPress={() => toggleDrawer()}>
+        Меню
+      </Text>
+    ),
   });
 
+  const toggleDrawer = () => {
+    setDrawer(!drawer);
+  };
+
+  const toReviews = () => {
+    navigation.navigate("ReviewsScreen");
+    toggleDrawer();
+  };
+
+  const toFAQ = () => {
+    navigation.navigate("FAQScreen");
+    toggleDrawer();
+  };
+
+  const toSizeChart = () => {
+    navigation.navigate("SizeChartScreen");
+    toggleDrawer();
+  };
 
   return (
-    <View>
-      <Text>MAINPAGE</Text>
-    </View>
-    // <Tab.Navigator
-    //   tabBarOptions={{
-    //     activeTintColor: "red",
-    //     activeBackgroundColor: "black",
-    //     showLabel: false,
-    //     // tabStyle: {
-    //     //   width: 100,
-    //     //   height: 100,
-    //     //   bottom: 30,
-    //     // },
-    //     style: {
-    //       // backgroundColor: 'blue',
-    //       height: 80,
-    //     },
-    //   }}
-    //   inactiveBackgroundColor="red"
-    // >
-    //   <Tab.Screen
-    //     options={{
-    //       tabBarIcon: ({ focused, size, color }) => (
-    //         <AntDesign
-    //           name="laptop"
-    //           size={focused ? 56 : 40}
-    //           color={focused ? "white" : "#120136"}
-    //         />
-    //       ),
-    //     }}
-    //     name="Main"
-    //     component={MainTab}
-    //   />
-    //   <Tab.Screen
-    //     options={{
-    //       tabBarIcon: ({ focused, size, color }) => (
-    //         <MaterialIcons
-    //           name="work"
-    //           size={focused ? 56 : 40}
-    //           color={focused ? "white" : "#fcbf1e"}
-    //         />
-    //       ),
-    //     }}
-    //     name="Услуги"
-    //     component={ServicesTab}
-    //   />
-    //   <Tab.Screen
-    //     options={{
-    //       tabBarIcon: ({ focused, size, color }) => (
-    //         <AntDesign
-    //           name="team"
-    //           size={focused ? 56 : 40}
-    //           color={focused ? "white" : "#035aa6"}
-    //         />
-    //       ),
-    //     }}
-    //     name="О нас"
-    //     component={AboutTab}
-    //   />
-    //   <Tab.Screen
-    //     options={{
-    //       tabBarIcon: ({ focused, size, color }) => (
-    //         <FontAwesome5
-    //           name="blog"
-    //           size={focused ? 56 : 40}
-    //           color={focused ? "white" : "#40bad5"}
-    //         />
-    //       ),
-    //     }}
-    //     name="Блог"
-    //     component={BlogTab}
-    //   />
-    // </Tab.Navigator>
+    <>
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: true,
+        }}
+      >
+        <Tab.Screen
+          // options={{
+          //   tabBarIcon: ({ focused, size, color }) => (
+          //     <AntDesign
+          //       name="laptop"
+          //       size={focused ? 56 : 40}
+          //       color={focused ? "white" : "tomato"}
+          //     />
+          //   ),
+          // }}
+          name="Home"
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          // options={{
+          //   tabBarIcon: ({ focused, size, color }) => (
+          //     <AntDesign
+          //       name="laptop"
+          //       size={focused ? 56 : 40}
+          //       color={focused ? "white" : "tomato"}
+          //     />
+          //   ),
+          // }}
+          name="Backet"
+          component={BacketScreen}
+        />
+        <Tab.Screen
+          // options={{
+          //   tabBarIcon: ({ focused, size, color }) => (
+          //     <AntDesign
+          //       name="laptop"
+          //       size={focused ? 56 : 40}
+          //       color={focused ? "white" : "tomato"}
+          //     />
+          //   ),
+          // }}
+          name="Profile"
+          component={ProfileScreen}
+        />
+      </Tab.Navigator>
+      {drawer ? (
+        <View style={styles.menu}>
+          <TouchableOpacity onPress={() => toReviews()} style={styles.menuItem}>
+            <Text>Отзывы</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => toFAQ()} style={styles.menuItem}>
+            <Text>FAQ</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => toSizeChart()}
+            style={styles.menuItem}
+          >
+            <Text>Размерная сетка</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
@@ -108,9 +131,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // fontFamily: "ubuntu-regular",
-
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  menu: {
+    backgroundColor: "lightblue",
+    width: 300,
+    height: 300,
+    position: "absolute",
+    top: "10%",
+    left: 0,
+  },
+  menuItem: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "grey",
+  },
+  menuWrapper: {
+    width: "100%",
+    height: "100%",
   },
 });
---
+++ screens/Menu/HamburgerMenu.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, } from "react-native";
+
+
+export const HamburgerMenu = () => {
+    return (
+        <Text>
+            I am menu
+        </Text>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      fontFamily: "ubuntu-regular",
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
