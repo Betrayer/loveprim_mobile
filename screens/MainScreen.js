eb972f700582a76da13b59b6a617f6ee34e44b9f
Betrayer <StarDestroyer@ukr.net>
2020-07-08 15:05:54 +0300

    item page

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -148,7 +148,7 @@ export const MainScreen = ({ navigation, route }) => {
       // console.log(token);
       // this.setState({ expoPushToken: token });
     } else {
-      alert("Must use physical device for Push Notifications");
+      // alert("Must use physical device for Push Notifications");
     }
 
     if (Platform.OS === "android") {
