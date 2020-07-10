6a10425814a001adebc40535b7ce4d969a05816b
Betrayer <StarDestroyer@ukr.net>
2020-07-10 10:29:14 +0300

    accrodion

--- screens/additionalScreens/ShoesScreen.js
+++ screens/additionalScreens/ShoesScreen.js
@@ -16,6 +16,10 @@ export const ShoesScreen = () => {
   const [exchange, setExchange] = useState("27");
   const [rate, setRate] = useState("27");
   const [allProducts, setAllProducts] = useState([]);
+  const [kid, setKid] = useState(false);
+  const [man, setMan] = useState(false);
+  const [woman, setWoman] = useState(false);
+  const [filter, setFilter] = useState(false);
 
   const renderedSeparator = () => {
     return <View style={styles.separator} />;
@@ -66,6 +70,9 @@ export const ShoesScreen = () => {
       setStorageRate();
     }
     setExchange(rate);
+    setWoman(false);
+    setMan(false);
+    setKid(false);
   }, []);
 
   useEffect(() => {
@@ -88,31 +95,74 @@ export const ShoesScreen = () => {
     }
   };
 
+  const womanSort = () => {
+    setWoman(!woman);
+    setKid(false);
+    setMan(false);
+  };
+
+  const manSort = () => {
+    setWoman(false);
+    setKid(false);
+    setMan(!man);
+  };
+
+  const kidSort = () => {
+    setWoman(false);
+    setKid(!kid);
+    setMan(false);
+  };
+
   return (
-    <FlatList
-      showsVerticalScrollIndicator={false}
-      activeOpacity={0.7}
-      data={allProducts}
-      keyExtractor={(item, index) => index.toString()}
-      ItemSeparatorComponent={renderedSeparator}
-      renderItem={({ item }) => {
-        return (
-          <TouchableOpacity
-            style={styles.container}
-            onPress={() => navigation.navigate("ItemScreen", { info: item })}
-          >
-            <Image
-              style={styles.pic}
-              source={{
-                uri: item.image,
-              }}
-            />
-            <Text>{item.name}</Text>
-            <Text>{item.price}</Text>
-          </TouchableOpacity>
-        );
-      }}
-    />
+    <>
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity
+          rounded
+          style={styles.button}
+          onPress={() => womanSort()}
+        >
+          <Text>Женщинам</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          rounded
+          style={styles.button}
+          onPress={() => manSort()}
+        >
+          <Text>Мужчинам</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          rounded
+          style={styles.button}
+          onPress={() => kidSort()}
+        >
+          <Text>Детям</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        activeOpacity={0.7}
+        data={allProducts}
+        keyExtractor={(item, index) => index.toString()}
+        ItemSeparatorComponent={renderedSeparator}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              style={styles.container}
+              onPress={() => navigation.navigate("ItemScreen", { info: item })}
+            >
+              <Image
+                style={styles.pic}
+                source={{
+                  uri: item.image,
+                }}
+              />
+              <Text>{item.name}</Text>
+              <Text>{item.price}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </>
   );
 };
 const styles = StyleSheet.create({
@@ -131,5 +181,20 @@ const styles = StyleSheet.create({
     height: 1,
     width: "100%",
     backgroundColor: "black",
+  },
+  buttonWrapper: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "30%",
+    height: 30,
+    backgroundColor: "lightsalmon",
+    marginTop: 20,
+    marginBottom: 20,
+    borderRadius: 6,
   },
 });
