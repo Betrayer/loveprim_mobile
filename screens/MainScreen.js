e1ff4132e6a43f1c2ccbec599ef7da8b086e0113
Alexlogvin2019 <57359889+Alexlogvin2019@users.noreply.github.com>
2020-07-13 14:28:28 +0300

    Alexdev (#25)
    * ver1
    
    * ver2
    
    * ver4
    
    * ver2
    
    * ver2
    
    * ver4
    
    * ver2
    
    * ver2
    
    * ver2
    
    Co-authored-by: Betrayer <58057555+Betrayer@users.noreply.github.com>

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -5,15 +5,17 @@ import {
   Text,
   View,
   Button,
+  Image,
   TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
+  TextInput,
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { AntDesign } from "@expo/vector-icons";
 import { Notifications } from "expo"; // Богдан тест
 import * as Permissions from "expo-permissions";
+// import { FontAwesome5 } from "@expo/vector-icons";
 import Constants from "expo-constants";
 import firebase from "firebase"; // Богдан тест
 import { useDispatch, useSelector } from "react-redux";
@@ -21,6 +23,7 @@ import { logoutUser } from "../redux/operations";
 import { ProfileScreen } from "./additionalScreens/ProfileScreen";
 import { BacketScreen } from "./additionalScreens/BacketScreen";
 import { HomeScreen } from "./additionalScreens/HomeScreen";
+// import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -29,7 +32,6 @@ export const MainScreen = ({ navigation, route }) => {
   const dispatch = useDispatch();
   const [allProducts, setAllProducts] = useState([]);
   const [user, setUser] = useState({});
-
   const [drawer, setDrawer] = useState(false);
 
   const logout = () => {
@@ -63,6 +65,9 @@ export const MainScreen = ({ navigation, route }) => {
   const toggleDrawer = () => {
     setDrawer(!drawer);
   };
+
+
+
   const getUser = async () => {
     await firebase
       .firestore()
@@ -131,7 +136,6 @@ export const MainScreen = ({ navigation, route }) => {
     navigation.navigate("SalesScreen");
     toggleDrawer();
   };
-
   // -=-=-=-=-=-=-=-=-=
 
   const registerForPushNotificationsAsync = async () => {
@@ -164,7 +168,7 @@ export const MainScreen = ({ navigation, route }) => {
       // console.log(token);
       // this.setState({ expoPushToken: token });
     } else {
-      alert("Must use physical device for Push Notifications");
+      // alert("Must use physical device for Push Notifications");
     }
 
     //   if (Platform.OS === "android") {
@@ -196,26 +200,8 @@ export const MainScreen = ({ navigation, route }) => {
 
   return (
     <>
-      <View
-        style={{
-          // justifyContent: "center",
-          alignItems: "center",
-          // marginBottom: -20
-        }}
-      >
-        {userId ? (
-          <TouchableOpacity
-            style={styles.buttonStl}
-            onPress={() => {
-              navigation.navigate("AdminPageScreen");
-            }}
-          >
-            <Text style={styles.buttonStlText}>ПАНЕЛЬ АДМИНИСТРАТОРА</Text>
-          </TouchableOpacity>
-        ) : (
-          <></>
-        )}
-      </View>
+ 
+
       <Tab.Navigator
         tabBarOptions={{
           showLabel: true,
@@ -337,14 +323,5 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
-  buttonStl: {
-    width: "80%",
-    height: 30,
-    borderRadius: 10,
-    backgroundColor: "#6CC4C7",
-    justifyContent: "center",
-    alignItems: "center",
-    // marginBottom: -40,
-    marginTop: 20,
-  },
+
 });
