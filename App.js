35c30f22c23048caf5588ba6bbe8125f55feb4e9
ellie25v <58464480+ellie25v@users.noreply.github.com>
2020-07-14 13:31:19 +0300

    Notities (#32)
    * update push token
    
    * some of notification added
    
    * fighting w ionicons
    
    * to merge
    
    * to merge
    
    Co-authored-by: Ellie Yelizarieva <ellie@Ellies-MacBook-Pro.local>
    Co-authored-by: Ellie Yelizarieva <ellie@Ellies-MBP.MyHost>

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
+import * as Font from 'expo-font';
 import firebase from "firebase"; // Богдан тест
 import { firestore, storage, auth, firebaseConfig } from "./firebase/config";
 import { store } from "./redux/store";
@@ -31,24 +32,39 @@ import { BoysScreen} from "./screens/additionalScreens/BoysScreen/BoysScreen"
 export default function App() {
   const Stack = createStackNavigator();
   const [isAuth, setIsAuth] = useState(false);
-  const [isReady, setIsReady] = useState(false);
 
-  useEffect(() => {
-    fontLoad()
-  }, [])
-  useEffect(() => {
-  }, [isReady])
+  // useEffect(() => {
+  //   useFonts({
+  //     'Bold': require('./assets/fonts/roboto-condensed-v18-latin_cyrillic-700.ttf'),
+  //     'Light': require('./assets/fonts/roboto-condensed-v18-latin_cyrillic-300.ttf'),
+  //     'Regular': require('./assets/fonts/roboto-condensed-v18-latin_cyrillic-regular.ttf'),
+  //     'Fjalla-One': require('./assets/fonts/fjalla-one-v7-latin-regular.ttf'),
+  //   })
+  // }, [])
 
-  const fontLoad = async() => {
-    await Expo.Font.loadAsync({
-      ionicons: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf'),
-  });
-  return setIsReady(true)
+
+  function useFonts(fontMap) {
+    let [fontsLoaded, setFontsLoaded] = useState(false);
+    (async () => {
+      await Font.loadAsync(fontMap);
+      setFontsLoaded(true);
+    })();
+  return [fontsLoaded];
   }
-  
+  let [fontsLoaded] = useFonts({
+    'Roboto-Condensed-Bold': require('./assets/fonts/Roboto-Condensed-Bold.ttf'),
+    'Roboto-Condensed-Light': require('./assets/fonts/Roboto-Condensed-Light.ttf'),
+    "Roboto-Condensed-Regular": require('./assets/fonts/Roboto-Condensed-Regular.ttf'),
+    Fjalla: require('./assets/fonts/Fjalla.ttf'),
+    ionicons: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf'),
+  });
+  if (!fontsLoaded) {
+    return <Image style={{alignSelf: 'stretch', height: 300, marginTop: 200}}
+     source={{uri : 'https://i.pinimg.com/originals/78/e8/26/78e826ca1b9351214dfdd5e47f7e2024.gif'}}/>;
+  } else {
   return (
     <Provider store={store}>
-      {isReady ? <NavigationContainer>
+       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
             // options={{ headerShown: false }}
@@ -57,6 +73,7 @@ export default function App() {
               headerTitle: "LovePRIM",
               headerStyle: {
                 backgroundColor: "#6CC4C7",
+                fontFamily: "Roboto-Condensed-Bold",
               },
               headerTintColor: "white",
             }}
@@ -292,10 +309,10 @@ export default function App() {
             component={BoysScreen}
           />
         </Stack.Navigator>
-      </NavigationContainer> : <></>}
+      </NavigationContainer> 
     </Provider>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
