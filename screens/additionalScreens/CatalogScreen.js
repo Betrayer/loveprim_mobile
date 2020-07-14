82314a2772d943dd145bd5d672e6cd4507d9e9a6
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-07-14 17:33:24 +0300

    ver with catalog

--- screens/additionalScreens/CatalogScreen.js
+++ screens/additionalScreens/CatalogScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
 export const CatalogScreen = ({ navigation }) => {
   const toReviews = () => {
@@ -55,43 +55,58 @@ export const CatalogScreen = ({ navigation }) => {
     <View style={styles.container}>
       <View style={styles.menu}>
         <TouchableOpacity onPress={() => toReviews()} style={styles.menuItem}>
+          <Image
+            style={styles.pic}
+            source={require("../../image/icons8-feedback-96.png")}
+          />
           <Text>Отзывы</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => toFAQ()} style={styles.menuItem}>
+          <Image style={styles.pic} source={require("../../image/faq.png")} />
           <Text>FAQ</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => toSizeChart()} style={styles.menuItem}>
-          <Text>Размерная сетка</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => toChildren()} style={styles.menuItem}>
-          <Text>Дети</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => toMen()} style={styles.menuItem}>
-          <Text>Мужчины</Text>
+          <Image style={styles.pic} source={require("../../image/sizes.png")} />
+          <Text>Размеры</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => toWomen()} style={styles.menuItem}>
-          <Text>Женщинцы</Text>
+          <Image style={styles.pic} source={require("../../image/woman.png")} />
+          <Text>Женщинам</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => toMen()} style={styles.menuItem}>
+          <Image style={styles.pic} source={require("../../image/man.png")} />
+          <Text>Мужчинам</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => toChildren()} style={styles.menuItem}>
+          <Image style={styles.pic} source={require("../../image/girl.png")} />
+          <Text>Девочкам</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => toBoys()} style={styles.menuItem}>
+          <Image style={styles.pic} source={require("../../image/boy.png")} />
+          <Text>Мальчикам</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => toShoes()} style={styles.menuItem}>
+          <Image style={styles.pic} source={require("../../image/shoes.png")} />
           <Text>Обувь</Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => toAccesories()}
           style={styles.menuItem}
         >
+          <Image style={styles.pic} source={require("../../image/accessory.png")} />
           <Text>Аксессуары</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => toDecor()} style={styles.menuItem}>
+          <Image style={styles.pic} source={require("../../image/decor.png")} />
           <Text>Декор</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => toStock()} style={styles.menuItem}>
-          <Text>Товар в наличии</Text>
+          <Image style={styles.pic} source={require("../../image/girl.png")} />
+          <Text>В наличии</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => toSales()} style={styles.menuItem}>
+          <Image style={styles.pic} source={require("../../image/stock.png")} />
           <Text>Скидки</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => toBoys()} style={styles.menuItem}>
-          <Text>Мальчикам</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -105,20 +120,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
-    padding: 10,    
+    padding: 10,
   },
   menu: {
     flexDirection: "row",
     flexWrap: "wrap",
     alignItems: "center",
     justifyContent: "space-between",
-
   },
   menuItem: {
     height: 100,
     width: 100,
-    borderColor: "red",
-    borderWidth: 2,
+    // borderColor: "red",
+    // borderWidth: 2,
     margin: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  pic: {
+    width: 80,
+    height: 80,
   },
 });
