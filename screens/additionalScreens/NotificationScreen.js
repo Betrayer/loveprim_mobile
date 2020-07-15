6a069ea7b65b2c88e9892e34714cee6f5c81b042
Ellie Yelizarieva <ellie@Ellies-MacBook-Pro.local>
2020-07-15 14:06:31 +0300

    new icons catalog

--- screens/additionalScreens/NotificationScreen.js
+++ screens/additionalScreens/NotificationScreen.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { SwipeListView } from "react-native-swipe-list-view";
 import { useSelector } from "react-redux";
+import { Ionicons } from "@expo/vector-icons";
 import { firestore } from "../../firebase/config";
 
 export const NotificationScreen = ({ navigation }) => {
@@ -18,8 +26,8 @@ export const NotificationScreen = ({ navigation }) => {
       .onSnapshot((data) => {
         setNotificationList(
           data.docs
-            .map((doc) => {
-              return { ...doc.data(), id: doc.id };
+            .map((doc, ind) => {
+              return { ...doc.data(), id: doc.id, key: { ind } };
             })
             .sort(function (a, b) {
               if (a.date > b.date) {
@@ -33,28 +41,60 @@ export const NotificationScreen = ({ navigation }) => {
         );
       });
   };
+  const closeRow = (rowMap, rowKey) => {
+    if (rowMap[rowKey]) {
+      rowMap[rowKey].closeRow();
+    }
+  };
+  const deleteNotification = async (id) => {
+    await firestore.collection("notifications").doc(id).delete();
+  };
 
+  const deleteRow = (rowMap, rowKey, id) => {
+    closeRow(rowMap, rowKey);
+    const newData = [...notificationList];
+    const prevIndex = notificationList.findIndex((item) => item.key === rowKey);
+    newData.splice(prevIndex, 1);
+    setNotificationList(newData);
+    deleteNotification(id)
+  };
+  const renderHiddenItem = (data, rowMap) => (
+    <View style={styles.rowBack}>
+      {/* <Text>Left</Text> */}
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnRight]}
+        onPress={() => deleteRow(rowMap, data.item.key, data.item.id)}
+      >
+        <Ionicons name="ios-trash" size={30} color="#fff"></Ionicons>
+      </TouchableOpacity>
+    </View>
+  );
+  const renderItem = (item) => (
+    <View style={styles.rowFront} underlayColor={"#AAA"}>
+      {console.log("item", item)}
+      <Text style={styles.notif}>
+        {item.item.notification}
+        <Text styles={styles.notifTextNo}>No{item.item.orderNo}</Text>
+      </Text>
+    </View>
+  );
+  const onRowDidOpen = (rowKey) => {
+    console.log("This row opened", rowKey);
+  };
   return (
     <View style={styles.container}>
-      {/* <Text></Text> */}
 
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        style={{ marginTop: 10 }}
-        horizontal={false}
-        activeOpacity={0.1}
+      <SwipeListView
         data={notificationList}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => {
-          return (
-            <View>
-              <Text style={styles.notif}>
-                {item.notification}
-                <Text styles={styles.notifTextNo}>No{item.orderNo}</Text>
-              </Text>
-            </View>
-          );
-        }}
+        style={{ marginTop: 20 }}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        leftOpenValue={0}
+        rightOpenValue={-50}
+        previewRowKey={"0"}
+        previewOpenValue={-40}
+        previewOpenDelay={3000}
+        onRowDidOpen={onRowDidOpen}
       />
     </View>
   );
@@ -62,14 +102,67 @@ export const NotificationScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
+    // flex: 1,
+    // backgroundColor: "#F5F8F8",
+    // alignItems: "center",
+    // justifyContent: "center",
+    // width: "100%",
+    backgroundColor: "white",
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    padding: 10,
+    paddingVertical: 10,
   },
-  notif:{
-    textAlign: 'center',
-  }
+  //   notifWrapper: {
+  //     alignSelf: "stretch",
+  //     paddingHorizontal: 12,
+  //     paddingVertical: 16,
+  //     marginVertical: 1,
+  //     backgroundColor: "#fff",
+  //     // marginHorizontal: 10,
+
+  //     // borderRadius:5,
+  //   },
+  notif: {
+    textAlign: "center",
+    fontFamily: "Roboto-Condensed-Regular",
+    fontSize: 18,
+    lineHeight: 25,
+    color: "#444",
+  },
+  notifTextNo: {
+    color: "#2f8f85",
+  },
+  backTextWhite: {
+    color: "#FFF",
+  },
+  rowFront: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderBottomColor: "#eee",
+    borderBottomWidth: 1,
+    justifyContent: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 26,
+  },
+  rowBack: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 15,
+  },
+  backRightBtn: {
+    alignItems: "center",
+    bottom: 0,
+    justifyContent: "center",
+    position: "absolute",
+    top: 0,
+    width: 50,
+  },
+
+  backRightBtnRight: {
+    backgroundColor: "tomato",
+    right: 0,
+    height: "100%",
+  },
 });
